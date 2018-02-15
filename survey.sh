cef1005c006557a2476a9d452aa7dc05be24d845
Alexander Kune <alexander@kune.de>
2018-02-15 07:53:03 +0100

    remove tmp files at startup and allow to configure annotation

--- survey.sh
+++ survey.sh
@@ -1,4 +1,6 @@
 startTime=$(date +%s)
+camerasRoot="/cameras"
+find $camerasRoot -name "*.tmp" -exec rm {} \;
 while true; do
   source /conf/cameras.conf
   for camera in $cameras; do
@@ -8,8 +10,9 @@ while true; do
     password="${camera}_password"; password=${!password}
     conversionInterval="${camera}_conversionInterval"; conversionInterval=${!conversionInterval:-3600}
     echo "$camera ($name) / $url"
+    annotation="${camera}_annotation"; annotation=${!annotation:-"$name %Y-%m-%d\ %H:%M:%S\ \(%Z\)"}
 
-    prefix=/cameras/${camera}
+    prefix=$camerasRoot/${camera}
     if [ ! -d $prefix ]; then
       mkdir -p $prefix
     fi
@@ -17,11 +20,12 @@ while true; do
     d=$(date +%Y.%m.%d-%H:%M:%S)
     #echo "Date: $d"
     date=$(date --date="$d" +%Y-%m-%d-%H-%M-%S)
+    annotation=$(eval echo "$annotation")
     filename=$prefix/$date.jpg
     tmpfilename=$filename.tmp
     ( (/usr/bin/wget $url -O $tmpfilename --user=$user --password=$password && \
-     imgDate=`date --date="$d" +%Y-%m-%d\ %H:%M:%S\ \(%Z\)` && \
-     convert "$tmpfilename" -gravity NorthWest -pointsize 22 -fill white -annotate +30+30 "$imgDate" "$tmpfilename") && mv $tmpfilename $filename && (cd /cameras; ln -sf ${camera}/${date}.jpg ${camera}.jpg) ) &
+     imgDate=`date --date="$d" +"$annotation"` && \
+     convert "$tmpfilename" -gravity NorthWest -pointsize 22 -fill white -annotate +30+30 "$imgDate" "$tmpfilename") && mv $tmpfilename $filename && (cd $camerasRoot; ln -sf ${camera}/${date}.jpg ${camera}.jpg) ) &
     lastConversion="${camera}_lastConversion"; lastConversion=${!lastConversion:-$startTime}
     if [ $(expr $(date +%s) - $lastConversion) -gt $conversionInterval ]; then
       echo Triggering converson
